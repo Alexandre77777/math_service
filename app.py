c25be2c8fb936e9ac23dae30a8f99b9510e7321f
alexandre <sa.32@m.ru>
2025-04-30 14:06:28 +0300

    Second

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 
 # URL бэкенда на render.com
 # Замените на свой URL после развертывания на render.com
-BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "https://math-service.onrender.com"
 
 # Функции для взаимодействия с бэкендом
 def check_api_status():
